50be919a38479129315a54283ec0f20b7bc0855e
Fenrir41 <151402592+Fenrir41@users.noreply.github.com>
2023-11-18 21:43:40 +0100

    Update Etudiant.py

--- Etudiant.py
+++ Etudiant.py
@@ -7,5 +7,23 @@ class Etudiant:
 		self.login = login
 		self.password = password
 		self.suspended = suspended
+		self.livres_empruntes = []
 	def __str__(self):
 		return f"{self.id},{self.nom},{self.prenom},{self.email},{self.login},{self.password},{self.suspended} \n"
+
+
+def emprunter_livre(self, livre):
+        if len(self.livres_empruntes) < 3:
+            	self.livres_empruntes.append(livre)
+            	return True
+        else:
+            	return False
+    
+def rendre_livre(self, livre):
+	if livre in self.livres_empruntes:
+		self.livres_empruntes.remove(livre)
+            	return True
+        else:
+           	return False
+
+
